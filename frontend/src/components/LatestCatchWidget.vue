228f29ceccc79fd215ce1146db24ac7f84007f23
Ubiquitous-X <ubiquitous@bahnhof.se>
2024-08-04 17:09:27 +0200

    minor tweaks

--- frontend/src/components/LatestCatchWidget.vue
+++ frontend/src/components/LatestCatchWidget.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="flex-grow hidden md:flex p-2 flex-col text-neutral-content">
-    <h2 class="font-bold text-lg mt-5">Senaste fångst:</h2>
-    <p v-if="latestCatch" class="text-xs">{{ formatDate(latestCatch.timestamp) }}</p>
-    <div v-if="latestCatch" class="latest-catch flex items-center">
+    <h2 class="font-bold text-lg mt-5">Senaste fångst</h2> 
+    <div v-if="latestCatch" class="latest-catch flex items-start">
       <div class="avatar mt-4">
-        <div class="mask rounded-xl h-16 w-16 lg:w-18 lg:h-18 lg:mr-2">
-          <img v-if="latestCatch.thumbnail_image_url" :src="latestCatch.thumbnail_image_url" alt="Senaste fångst bild" class="cursor-pointer" @click="openSingleFishView(latestCatch.slug)">
+        <div class="mask rounded-xl h-16 w-16 lg:w-20 lg:h-20 lg:mr-2">
+          <img
+            v-if="latestCatch.thumbnail_image_url"
+            :src="latestCatch.thumbnail_image_url"
+            alt="Senaste fångst bild"
+            class="cursor-pointer"
+            @click="openSingleFishView(latestCatch.slug)"
+          />
           <span v-else class="flex items-center justify-center h-full w-full bg-base-200 text-base-300">Fisk</span>
         </div>
       </div>
-      <div class="ml-4">
-        <p class="mt-4 text-sm"><strong>Art:</strong> {{ latestCatch.species.name }}</p>
+      <div class="ml-4 flex flex-col">
+        <p class="mt-4 text-sm"><strong>Tid:</strong> {{ formatDate(latestCatch.timestamp) }}</p>
+        <p class="text-sm"><strong>Art:</strong> {{ latestCatch.species.name }}</p>
         <p class="text-sm"><strong>Plats:</strong> {{ latestCatch.location }}</p>
         <p class="text-sm"><strong>Väder:</strong> {{ latestCatch.weather }}, {{ latestCatch.air_temperature !== null ? formatTemperature(latestCatch.air_temperature) : 'Saknas' }}</p>
       </div>
-    </div>
+    </div>    
     <div v-else>
       <p>Ingen senaste fångst tillgänglig.</p>
     </div>
